4a6eb358b40014efe5aba07e687f0d16b3db5993
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-05 20:30:13 -0300

    Update main.c

--- C/main.c
+++ C/main.c
@@ -76,8 +76,8 @@ int main() {
     double determinanteA = determinante(A);
     printf("\n O determinante da Matriz A é:\n %lf", determinanteA);
     
-    double determinanteA = determinante(A);
-    printf("\n O determinante da Matriz A é:\n %lf", determinanteA);
+    double determinanteA = determinante(B);
+    printf("\n O determinante da Matriz A é:\n %lf", determinanteB);
 
     
 
