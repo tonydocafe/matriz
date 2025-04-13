d9a512dbdc3a4f4634a024a553939d6632daff96
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-04-12 22:47:14 -0300

    Update Matriz.py

--- Py/Matriz.py
+++ Py/Matriz.py
@@ -3,3 +3,12 @@ class Matriz:
         self.m = m
         self.n = n
         self.matriz = [[0.0 for _ in range(n)] for _ in range(m)]
+  @classmethod
+    def ler_de_arquivo(cls, caminho):
+        with open(caminho, 'r') as f:
+            linhas = [linha.strip() for linha in f.readlines()]
+        tamanho = int(linhas.pop(0))
+        matriz = cls(tamanho, tamanho)
+        for i in range(tamanho):
+            matriz.matriz[i] = list(map(float, linhas[i].split()))
+        return matriz
